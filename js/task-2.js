fc262cabdb878238f08708d022bd212fbdb9a069
defaultnastia <anastayaaa@gmail.com>
2024-04-26 20:00:02 +0200

    update to task-1 and structure

--- js/task-2.js
+++ js/task-2.js
@@ -1,14 +1,1 @@
-'use strict';
 
-function makeArray(firstArray, secondArray, maxLength) {
-  const newArrayFull = firstArray.concat(secondArray);
-  const newArrayCropped = newArrayFull.slice(0, maxLength);
-  return newArrayFull.length > maxLength ? newArrayCropped : newArrayFull;
-}
-
-console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,1 @@
-'use strict';
 
-function filterArray(numbers, value) {
-  const arrayOfBigValues = [];
-  for (const numberElement of numbers) {
-    if (numberElement <= value) {
-      continue;
-    }
-    arrayOfBigValues.push(numberElement);
-  }
-  return arrayOfBigValues;
-}
-
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
